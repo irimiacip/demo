fc984f7df40a71b254ad5f53318dabaae68fff44
irimiacip <irimiacip@yahoo.com>
2020-10-14 11:17:36 +0300

    Update asd.java

--- src/main/java/asd.java
+++ src/main/java/asd.java
@@ -1,6 +1,7 @@
 public class asd {
 public static void main(String args []){
     System.out.println("test");
+    System.out.println("Changes made by user 1");
     System.out.println("test 1");
 }
 
