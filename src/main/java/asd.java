0a65c5a41c6a6c428ff532ae33cff7d2ce22d1fa
Ciprian-Andrei Irimia <cirimia@cbn.ro>
2020-10-14 11:31:14 +0300

    test

--- src/main/java/asd.java
+++ src/main/java/asd.java
@@ -3,7 +3,7 @@ public static void main(String args []){
     System.out.println("test");
     System.out.println("Changes made by user 1");
     System.out.println("test 1");
-    System.out.println("Changes made by user 2");
+    System.out.println("Changes made by user 24");
 }
 
 }
