54a5cfecd27ceaefd610b3cc5c18abadef55ce4e
irimiacip <irimiacip@yahoo.com>
2020-11-27 00:21:23 +0200

    sent from remote

--- src/main/java/asd.java
+++ src/main/java/asd.java
@@ -3,7 +3,7 @@ import java.sql.SQLOutput;
 public class asd {
 public static void main(String args []){
     System.out.println("push from remote");
-    System.out.println("psuh second from remote");
+    System.out.println("psuh   second   from   remote");
 }
 
 }
