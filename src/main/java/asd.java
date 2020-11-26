3f0022a69a6b4fc69a807246488650ace50f9b5b
irimiacip <irimiacip@yahoo.com>
2020-11-27 00:15:12 +0200

    sent from remote

--- src/main/java/asd.java
+++ src/main/java/asd.java
@@ -2,12 +2,8 @@ import java.sql.SQLOutput;
 
 public class asd {
 public static void main(String args []){
-    System.out.println("test");
-    System.out.println("Changes made by user 1");
-    System.out.println("test 1");
-    System.out.println("Changes made by user 2");
-    System.out.println("Changes by ciprian");
-    System.out.println("Changes by ciprian...sa vedem ");
+    System.out.println("push from remote");
+
 }
 
 }
