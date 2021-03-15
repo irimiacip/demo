b11b1a8ac7efe55109c92a5e3431597e298272c2
Ciprian-Andrei Irimia <cirimia@cbn.ro>
2021-03-15 15:30:04 +0200

    push to FEATURE

--- src/main/java/asd.java
+++ src/main/java/asd.java
@@ -5,7 +5,7 @@ public static void main(String args []){
     System.out.println("push from remote");
     System.out.println("psuh   second   from   remote");
     System.out.println("test");
-    System.out.println("sent code to feature");
+    System.out.println("sent code to suport");
 }
 
 }
