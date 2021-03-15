fe4cbced1c1d32bcc9af1c054cb84196ca892f9b
Ciprian-Andrei Irimia <cirimia@cbn.ro>
2021-03-15 15:25:40 +0200

    push to DEV

--- src/main/java/asd.java
+++ src/main/java/asd.java
@@ -5,6 +5,7 @@ public static void main(String args []){
     System.out.println("push from remote");
     System.out.println("psuh   second   from   remote");
     System.out.println("test");
+    System.out.println("sent code to DEV branch");
 }
 
 }
