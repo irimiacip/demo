8b77cfc87f94124f1a30aa6228a91c5d8e629cd7
Ciprian-Andrei Irimia <cirimia@cbn.ro>
2021-03-15 15:19:26 +0200

    push TEST

--- src/main/java/asd.java
+++ src/main/java/asd.java
@@ -4,6 +4,7 @@ public class asd {
 public static void main(String args []){
     System.out.println("push from remote");
     System.out.println("psuh   second   from   remote");
+    System.out.println("test");
 }
 
 }
